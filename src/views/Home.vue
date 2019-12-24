3291a3fcfe323bf557e104cd3085a7e2ab2566f5
Peter Jang <peter.x.jang@gmail.com>
2019-12-23 20:28:21 -0600

    Use Vue.js data to create markers and popups

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,11 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      places: [
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+        { lat: -33.8675, lng: 151.207, description: "The main city!" }
+      ]
     };
   },
   created: function() {},
@@ -26,14 +30,41 @@ export default {
     var map = new mapboxgl.Map({
       container: "map", // container id
       style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-      center: [-87.6, 41.8], // starting position [lng, lat]
-      zoom: 5 // starting zoom
+      center: [this.places[0].lng, this.places[0].lat], // starting position [lng, lat]
+      zoom: 3 // starting zoom
     });
-    var popup = new mapboxgl.Popup({ offset: 25 }).setText("Construction on the Washington Monument began in 1848.");
-    var marker = new mapboxgl.Marker()
-      .setLngLat([-87.6, 41.8])
-      .setPopup(popup)
-      .addTo(map);
+    // WITHOUT A LOOP
+    // var popup = new mapboxgl.Popup({ offset: 25 }).setText(this.places[0].description);
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([this.places[0].lng, this.places[0].lat])
+    //   .setPopup(popup)
+    //   .addTo(map);
+    // var popup2 = new mapboxgl.Popup({ offset: 25 }).setText(this.places[1].description);
+    // var marker2 = new mapboxgl.Marker()
+    //   .setLngLat([this.places[1].lng, this.places[1].lat])
+    //   .setPopup(popup2)
+    //   .addTo(map);
+
+    // WITH A WHILE LOOP
+    // var index = 0;
+    // while (index < this.places.length) {
+    //   var place = this.places[index];
+    //   var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+    //   var marker = new mapboxgl.Marker()
+    //     .setLngLat([place.lng, place.lat])
+    //     .setPopup(popup)
+    //     .addTo(map);
+    //   index += 1;
+    // }
+
+    // WITH A forEach LOOP
+    this.places.forEach(function(place) {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      var marker = new mapboxgl.Marker()
+        .setLngLat([place.lng, place.lat])
+        .setPopup(popup)
+        .addTo(map);
+    });
   },
   methods: {}
 };
