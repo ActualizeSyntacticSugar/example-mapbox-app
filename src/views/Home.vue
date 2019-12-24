376a4deb7e9b45239e36a12db0ff4492c74aad12
Peter Jang <peter.x.jang@gmail.com>
2019-12-23 20:58:04 -0600

    Use geocoding to convert address strings into lat/lng for markers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,58 +12,45 @@
 </style>
 
 <script>
-/* global mapboxgl */
+/* global mapboxgl, mapboxSdk */
 
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
       places: [
-        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
-        { lat: -33.8675, lng: 151.207, description: "The main city!" }
+        { address: "222 Merchandise Mart Plz, Chicago", description: "Merchandise Mart" },
+        { address: "215 W Ohio, Chicago", description: "Actualize" }
       ]
     };
   },
   created: function() {},
   mounted: function() {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+    var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+
     var map = new mapboxgl.Map({
-      container: "map", // container id
-      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-      center: [this.places[0].lng, this.places[0].lat], // starting position [lng, lat]
-      zoom: 3 // starting zoom
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-87.62, 41.88],
+      zoom: 12
     });
-    // WITHOUT A LOOP
-    // var popup = new mapboxgl.Popup({ offset: 25 }).setText(this.places[0].description);
-    // var marker = new mapboxgl.Marker()
-    //   .setLngLat([this.places[0].lng, this.places[0].lat])
-    //   .setPopup(popup)
-    //   .addTo(map);
-    // var popup2 = new mapboxgl.Popup({ offset: 25 }).setText(this.places[1].description);
-    // var marker2 = new mapboxgl.Marker()
-    //   .setLngLat([this.places[1].lng, this.places[1].lat])
-    //   .setPopup(popup2)
-    //   .addTo(map);
 
-    // WITH A WHILE LOOP
-    // var index = 0;
-    // while (index < this.places.length) {
-    //   var place = this.places[index];
-    //   var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
-    //   var marker = new mapboxgl.Marker()
-    //     .setLngLat([place.lng, place.lat])
-    //     .setPopup(popup)
-    //     .addTo(map);
-    //   index += 1;
-    // }
-
-    // WITH A forEach LOOP
     this.places.forEach(function(place) {
-      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
-      var marker = new mapboxgl.Marker()
-        .setLngLat([place.lng, place.lat])
-        .setPopup(popup)
-        .addTo(map);
+      mapboxClient.geocoding
+        .forwardGeocode({
+          query: place.address,
+          autocomplete: false,
+          limit: 1
+        })
+        .send()
+        .then(function(response) {
+          if (response && response.body && response.body.features && response.body.features.length) {
+            var feature = response.body.features[0];
+
+            new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+          }
+        });
     });
   },
   methods: {}
